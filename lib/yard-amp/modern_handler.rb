168b1af1edcd558e919a9f0cee8fd52952bf5994
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-05-12 14:39:11 -0400

    Basic handlers are defined and specced. Currently commands are created as subclasses of Amp::Command and their descriptions and help info is stored as metadata.

--- lib/yard-amp/modern_handler.rb
+++ lib/yard-amp/modern_handler.rb
@@ -20,12 +20,46 @@ module YARD::Amp
         o.superclass = P("Amp::Command")
         o.superclass.type = :class if o.superclass.is_a?(Proxy)
       end
-      p klass
+      klass[:amp_data] ||= {}
+
+      parse_block statement[2], owner: klass
     end
     
     def commands_module
       mod = register ModuleObject.new(:root, "Amp")
       com_mod = register ModuleObject.new(mod, "Commands")
+    end
+    
+    def parse_block(inner_node, opts = {})
+      push_state(opts) do
+        nodes = case inner_node.type
+                when :list then inner_node.children
+                when :do_block then inner_node.children[1]
+                else [inner_node]
+                end
+        parser.process(nodes)
+      end
+    end
+  end
+  
+  class ModernHelpHandler < YARD::Handlers::Ruby::Base
+    handles method_call(:help)
+    include YARD::CodeObjects
+    
+    def process
+      return unless owner.inheritance_tree(false).include?(P("Amp::Command"))
+      params = statement.parameters
+      owner[:amp_data].merge!(help: params.first.source)
+    end
+  end
+  
+  class ModernDescriptionHandler < YARD::Handlers::Ruby::Base
+    handles method_call(:desc)
+    include YARD::CodeObjects
+    
+    def process
+      params = statement.parameters
+      owner[:amp_data].merge!(desc: params.first.source)
     end
   end
 end
--- spec/yard-amp_spec.rb
+++ spec/yard-amp_spec.rb
@@ -4,13 +4,29 @@ describe ::YARD::Amp::ModernCommandHandler do
   include Helpers::Examples
   before(:all) do
     parse_file :simple_command
+    @init_cmd = Registry.at("Amp::Commands::Init")
   end
   
   it "creates commands when parsing a command declaration" do
-    Registry.at("Amp::Commands::Init").should_not be_nil
+    @init_cmd.should_not be_nil
   end
   
   it "assigns commands the superclass of Amp::Command" do
-    Registry.at("Amp::Commands::Init").superclass.path.should == "Amp::Command"
+    @init_cmd.superclass.path.should == "Amp::Command"
   end
+  
+  it "creates a metadata hash on amp command classes" do
+    @init_cmd[:amp_data].should_not be_nil
+  end
+  
+  it "extracts help information and attaches it as metadata" do
+    @init_cmd[:amp_data].should have_key(:help)
+    @init_cmd[:amp_data][:help].should == "%Q{Help info for init}"
+  end
+  
+  it "extracts description information and attaches it as metadata" do
+    @init_cmd[:amp_data].should have_key(:desc)
+    @init_cmd[:amp_data][:desc].should == %Q{"Initializes a new repository in the current directory."}
+  end
+  
 end
