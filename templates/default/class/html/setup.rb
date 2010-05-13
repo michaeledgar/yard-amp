1e18e9a630d16b4d60d8272b50b9eaf935012f8c
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-05-13 00:41:05 -0400

    Finally got templates rendering. Though they kinda look like ass now.

--- templates/default/class/html/setup.rb
+++
@@ -1,1 +0,0 @@
-include T('default/class/html')
--- templates/default/class/setup.rb
+++ templates/default/class/setup.rb
@@ -1,14 +1,15 @@
-include T('default/class')
-
 def init
   super
-  sections.last.place(:amp_options).before(:constant_summary)
+  sections.place(:amp_options).before(:constant_summary)
 end
 
-def option_listing
-  return @amp_options if @amp_options
-  return [] unless object.has_key?(:amp_data) && object[:amp_data].has_key?(:options)
-  @amp_options = object[:amp_data][:options].sort do |x, y|
-    x[:name] <=> y[:name]
+def amp_options
+  unless object[:amp_data] && object[:amp_data][:options]
+    @amp_options = [] 
+  else
+    @amp_options = object[:amp_data][:options].sort do |x, y|
+      x[:name] <=> y[:name]
+    end
   end
+  erb(:amp_options)
 end
